b4f90c6ed08f5f72f3df6f82e7af5857160bc7d4
Mauro Melo <mauromelo1@gmail.com>
2021-02-12 12:59:28 -0300

    LoginControlador com Alerts

--- src/gui/LoginControlador.java
+++ src/gui/LoginControlador.java
@@ -1,8 +1,11 @@
 package gui;
 
+
+import gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class LoginControlador {
 
@@ -13,8 +16,14 @@ public class LoginControlador {
 	private PasswordField campoSenha;
 
 	public void entrar() {
-		System.out.println(campoEmail.getText());
-		System.out.println(campoSenha.getText());
+		boolean emailValido = campoEmail.getText().equals("aluno@gmail.com");
+		boolean senhaValida = campoSenha.getText().equals("147258");
+
+		if (emailValido && senhaValida) {
+			Alerts.showAlert("Aviso", "", "Login realizado com Sucesso!", AlertType.INFORMATION);
+		} else {
+			Alerts.showAlert("Aviso", "", "Usuário e Senha inválidos!", AlertType.ERROR);
+		}
 	}
 
 }
